c4de8e882cdf6766e8d4a0de2ce6ee1ca0f570cd
Quang Hoang <quang@loginid.io>
2024-06-21 16:14:18 -0400

    locking

--- database/crate/create.go
+++ database/crate/create.go
@@ -13,10 +13,11 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"sync/atomic"
 	"time"
 
-	"github.com/golang-migrate/migrate/v4"
+	"go.uber.org/atomic"
+
+	migrate "github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database"
 	"github.com/golang-migrate/migrate/v4/database/multistmt"
 	"github.com/hashicorp/go-multierror"
@@ -35,6 +36,7 @@ var (
 	multiStmtDelimiter = []byte(";")
 
 	DefaultMigrationsTable       = "schema_migrations"
+	DefaultLockTable             = "schema_migrations_lock"
 	DefaultMultiStatementMaxSize = 10 * 1 << 20 // 10MB
 )
 
@@ -46,14 +48,12 @@ var (
 )
 
 type Config struct {
-	MigrationsTable       string
+	MigrationsSchemaName  string
+	MigrationsTableName   string
 	MigrationsTableQuoted bool
 	MultiStatementEnabled bool
-	MigrationsTableSchema string
+	LockTableName         string
 	DatabaseName          string
-	SchemaName            string
-	migrationsSchemaName  string
-	migrationsTableName   string
 	StatementTimeout      time.Duration
 	MultiStatementMaxSize int
 }
@@ -89,7 +89,7 @@ func WithConnection(ctx context.Context, conn *sql.Conn, config *Config) (*Crate
 		}
 	}
 
-	if config.SchemaName == "" {
+	if config.MigrationsSchemaName == "" {
 		// pull the schema name from the connection
 		query := `SELECT CURRENT_SCHEMA()`
 
@@ -103,25 +103,31 @@ func WithConnection(ctx context.Context, conn *sql.Conn, config *Config) (*Crate
 			return nil, ErrNoSchema
 		}
 
-		config.SchemaName = schemaName.String
+		config.MigrationsSchemaName = schemaName.String
 	}
 
-	config.migrationsSchemaName = config.SchemaName
-	config.migrationsTableName = config.MigrationsTable
 	if config.MigrationsTableQuoted {
 		re := regexp.MustCompile(`"(.*?)"`)
-		result := re.FindAllStringSubmatch(config.MigrationsTable, -1)
-		config.migrationsTableName = result[len(result)-1][1]
+		result := re.FindAllStringSubmatch(config.MigrationsTableName, -1)
+		config.MigrationsTableName = result[len(result)-1][1]
 		if len(result) == 2 {
-			config.migrationsSchemaName = result[0][1]
+			config.MigrationsSchemaName = result[0][1]
 		} else if len(result) > 2 {
-			return nil, fmt.Errorf("\"%s\" MigrationsTable contains too many dot characters", config.MigrationsTable)
+			return nil, fmt.Errorf("\"%s\" MigrationsTable contains too many dot characters", config.MigrationsTableName)
 		}
+	}
+
+	if config.MigrationsTableName == "" {
+		config.MigrationsTableName = DefaultMigrationsTable
 	}
 
 	crate := &Crate{
 		conn:   conn,
 		config: config,
+	}
+
+	if err := crate.ensureLockTable(); err != nil {
+		return nil, err
 	}
 
 	if err := crate.ensureVersionTable(); err != nil {
@@ -148,6 +154,7 @@ func WithInstance(instance *sql.DB, config *Config) (database.Driver, error) {
 		return nil, err
 	}
 	crate.db = instance
+
 	return crate, nil
 }
 
@@ -157,17 +164,23 @@ func (c *Crate) Open(connStr string) (database.Driver, error) {
 		return nil, err
 	}
 
-	db, err := sql.Open("crate", migrate.FilterCustomQuery(purl).String())
+	re := regexp.MustCompile("^(crate|cratedb)")
+	connectString := re.ReplaceAllString(migrate.FilterCustomQuery(purl).String(), "postgres")
+
+	db, err := sql.Open("postgres", connectString)
+	if err != nil {
+		return nil, err
+	}
 	migrationsTable := purl.Query().Get("x-migrations-table")
 	migrationsTableQuoted := false
-	if s := purl.Query().Get("x-migrations-table-quoted"); len(s) > 0 {
+	if s := purl.Query().Get("x-migrations-table-quoted"); s != "" {
 		migrationsTableQuoted, err = strconv.ParseBool(s)
 		if err != nil {
 			return nil, fmt.Errorf("invalid x-migrations-table-quoted: %w", err)
 		}
 	}
 
-	if (len(migrationsTable) > 0) && (migrationsTableQuoted) && ((migrationsTable[0] != '"') || (migrationsTable[len(migrationsTable)-1] != '"')) {
+	if (migrationsTable != "") && (migrationsTableQuoted) && ((migrationsTable[0] != '"') || (migrationsTable[len(migrationsTable)-1] != '"')) {
 		return nil, fmt.Errorf("x-migrations-table must be quoted (for instance '\"migrate\".\"schema_migrations\"') when x-migrations-table-quoted is enabled, current value is: %s", migrationsTable)
 	}
 
@@ -180,8 +193,13 @@ func (c *Crate) Open(connStr string) (database.Driver, error) {
 		}
 	}
 
+	lockTable := purl.Query().Get("x-lock-table")
+	if lockTable == "" {
+		lockTable = DefaultLockTable
+	}
+
 	multiStatementMaxSize := DefaultMultiStatementMaxSize
-	if s := purl.Query().Get("x-multi-statement-max-size"); len(s) > 0 {
+	if s := purl.Query().Get("x-multi-statement-max-size"); s != "" {
 		multiStatementMaxSize, err = strconv.Atoi(s)
 		if err != nil {
 			return nil, err
@@ -192,7 +210,7 @@ func (c *Crate) Open(connStr string) (database.Driver, error) {
 	}
 
 	multiStatementEnabled := false
-	if s := purl.Query().Get("x-multi-statement"); len(s) > 0 {
+	if s := purl.Query().Get("x-multi-statement"); s != "" {
 		multiStatementEnabled, err = strconv.ParseBool(s)
 		if err != nil {
 			return nil, fmt.Errorf("unable to parse option x-multi-statement: %w", err)
@@ -201,11 +219,12 @@ func (c *Crate) Open(connStr string) (database.Driver, error) {
 
 	crate, err := WithInstance(db, &Config{
 		DatabaseName:          purl.Path,
-		MigrationsTable:       migrationsTable,
+		MigrationsTableName:   migrationsTable,
 		MigrationsTableQuoted: migrationsTableQuoted,
 		StatementTimeout:      time.Duration(statementTimeout) * time.Millisecond,
 		MultiStatementEnabled: multiStatementEnabled,
 		MultiStatementMaxSize: multiStatementMaxSize,
+		LockTableName:         lockTable,
 	})
 
 	if err != nil {
@@ -229,15 +248,54 @@ func (c *Crate) Close() error {
 }
 
 func (c *Crate) Lock() error {
-	if c.isLocked.Swap(true) {
-		return database.ErrLocked
-	}
-	return nil
+	return database.CasRestoreOnErr(&c.isLocked, false, true, database.ErrLocked, func() (err error) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+
+		lockID, err := database.GenerateAdvisoryLockId(c.config.DatabaseName)
+		if err != nil {
+			return err
+		}
+
+		query := "SELECT COUNT(1) FROM " + pq.QuoteIdentifier(c.config.MigrationsSchemaName) + "." + pq.QuoteIdentifier(c.config.LockTableName) + " WHERE lock_id = $1"
+		row := c.conn.QueryRowContext(context.Background(), query, lockID)
+
+		var count int
+		err = row.Scan(&count)
+		if err != nil {
+			return &database.Error{OrigErr: err, Query: []byte(query)}
+		}
+		if count > 0 {
+			return database.ErrLocked
+		}
+
+		query = "INSERT INTO " + pq.QuoteIdentifier(c.config.MigrationsSchemaName) + "." + c.config.LockTableName + " (lock_id) VALUES ($1)"
+		if _, err = c.conn.ExecContext(ctx, query, lockID); err != nil {
+			return &database.Error{OrigErr: err, Query: []byte(query)}
+		}
+
+		return nil
+	})
 }
 
 func (c *Crate) Unlock() error {
-	c.isLocked.Store(false)
-	return nil
+	return database.CasRestoreOnErr(&c.isLocked, true, false, database.ErrNotLocked, func() (err error) {
+		lockID, err := database.GenerateAdvisoryLockId(c.config.DatabaseName)
+		if err != nil {
+			return err
+		}
+
+		// In the event of an implementation (non-migration) error, it is possible for the lock to not be released.  Until
+		// a better locking mechanism is added, a manual purging of the lock table may be required in such circumstances
+		query := "DELETE FROM " + pq.QuoteIdentifier(c.config.MigrationsSchemaName) + "." + c.config.LockTableName + " WHERE lock_id = $1"
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		if _, err := c.conn.ExecContext(ctx, query, lockID); err != nil {
+			return database.Error{OrigErr: err, Err: "failed to release migration lock", Query: []byte(query)}
+		}
+
+		return nil
+	})
 }
 
 func (c *Crate) Run(migration io.Reader) error {
@@ -271,13 +329,14 @@ func (c *Crate) runStatement(statement []byte) error {
 	if strings.TrimSpace(query) == "" {
 		return nil
 	}
+
 	if _, err := c.conn.ExecContext(ctx, query); err != nil {
 		if pgErr, ok := err.(*pq.Error); ok {
 			var line uint
 			var col uint
 			var lineColOK bool
 			if pgErr.Position != "" {
-				if pos, err := strconv.ParseUint(pgErr.Position, 10, 64); err == nil {
+				if pos, parseErr := strconv.ParseUint(pgErr.Position, 10, 64); parseErr == nil {
 					line, col, lineColOK = computeLineFromPos(query, int(pos))
 				}
 			}
@@ -295,9 +354,9 @@ func (c *Crate) runStatement(statement []byte) error {
 	return nil
 }
 
-func computeLineFromPos(s string, pos int) (line uint, col uint, ok bool) {
+func computeLineFromPos(s string, pos int) (line, col uint, ok bool) {
 	// replace crlf with lf
-	s = strings.Replace(s, "\r\n", "\n", -1)
+	s = strings.ReplaceAll(s, "\r\n", "\n")
 	// pg docs: pos uses index 1 for the first character, and positions are measured in characters not bytes
 	runes := []rune(s)
 	if pos > len(runes) {
@@ -331,16 +390,11 @@ func runesLastIndex(input []rune, target rune) int {
 }
 
 func (c *Crate) SetVersion(version int, dirty bool) error {
-	tx, err := c.conn.BeginTx(context.Background(), &sql.TxOptions{})
-	if err != nil {
-		return &database.Error{OrigErr: err, Err: "transaction start failed"}
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	query := `TRUNCATE ` + pq.QuoteIdentifier(c.config.migrationsSchemaName) + `.` + pq.QuoteIdentifier(c.config.migrationsTableName)
-	if _, err := tx.Exec(query); err != nil {
-		if errRollback := tx.Rollback(); errRollback != nil {
-			err = multierror.Append(err, errRollback)
-		}
+	query := `DELETE FROM ` + c.migrationsTableWithSchema() + ` WHERE version >= $1`
+	if _, err := c.conn.ExecContext(ctx, query, version); err != nil {
 		return &database.Error{OrigErr: err, Query: []byte(query)}
 	}
 
@@ -348,26 +402,20 @@ func (c *Crate) SetVersion(version int, dirty bool) error {
 	// empty schema version for failed down migration on the first migration
 	// See: https://github.com/golang-migrate/migrate/issues/330
 	if version >= 0 || (version == database.NilVersion && dirty) {
-		query = `INSERT INTO ` + pq.QuoteIdentifier(c.config.migrationsSchemaName) + `.` + pq.QuoteIdentifier(c.config.migrationsTableName) + ` (version, dirty) VALUES ($1, $2)`
-		if _, err := tx.Exec(query, version, dirty); err != nil {
-			if errRollback := tx.Rollback(); errRollback != nil {
-				err = multierror.Append(err, errRollback)
-			}
+		// TODO: For some reason this always returns a primary-key-conflict error without the ON CONFLICT clause
+		query = `INSERT INTO ` + c.migrationsTableWithSchema() + ` (version, dirty) VALUES ($1, $2)
+			ON CONFLICT (version) DO UPDATE SET dirty = $2
+		`
+		if _, err := c.conn.ExecContext(ctx, query, version, dirty); err != nil {
 			return &database.Error{OrigErr: err, Query: []byte(query)}
 		}
-	}
-
-	if err := tx.Commit(); err != nil {
-		return &database.Error{OrigErr: err, Err: "transaction commit failed"}
 	}
 
 	return nil
 }
 
 func (c *Crate) Version() (version int, dirty bool, err error) {
-	query := `SELECT version, dirty FROM ` +
-		pq.QuoteIdentifier(c.config.migrationsSchemaName) + `.` +
-		pq.QuoteIdentifier(c.config.migrationsTableName) + ` LIMIT 1`
+	query := `SELECT version, dirty FROM ` + c.migrationsTableWithSchema() + ` LIMIT 1`
 	err = c.conn.QueryRowContext(context.Background(), query).Scan(&version, &dirty)
 	switch {
 	case err == sql.ErrNoRows:
@@ -406,7 +454,7 @@ func (c *Crate) Drop() (err error) {
 		if err := tables.Scan(&tableName); err != nil {
 			return err
 		}
-		if len(tableName) > 0 {
+		if tableName != "" {
 			tableNames = append(tableNames, tableName)
 		}
 	}
@@ -428,7 +476,8 @@ func (c *Crate) Drop() (err error) {
 }
 
 func (c *Crate) ensureVersionTable() (err error) {
-	if err = c.Lock(); err != nil {
+	err = c.Lock()
+	if err != nil {
 		return err
 	}
 
@@ -447,7 +496,7 @@ func (c *Crate) ensureVersionTable() (err error) {
 	// `CREATE TABLE IF NOT EXISTS...` query would fail because the user does not have the CREATE permission.
 	// Taken from https://github.com/mattes/migrate/blob/master/database/postgres/postgres.go#L258
 	query := `SELECT COUNT(1) FROM information_schema.tables WHERE table_schema = $1 AND table_name = $2 LIMIT 1`
-	row := c.conn.QueryRowContext(context.Background(), query, c.config.migrationsSchemaName, c.config.migrationsTableName)
+	row := c.conn.QueryRowContext(context.Background(), query, c.config.MigrationsSchemaName, c.config.MigrationsTableName)
 
 	var count int
 	err = row.Scan(&count)
@@ -459,10 +508,20 @@ func (c *Crate) ensureVersionTable() (err error) {
 		return nil
 	}
 
-	query = `CREATE TABLE IF NOT EXISTS ` + pq.QuoteIdentifier(c.config.migrationsSchemaName) + `.` + pq.QuoteIdentifier(c.config.migrationsTableName) + ` (version bigint not null primary key, dirty boolean not null)`
+	query = `CREATE TABLE IF NOT EXISTS ` + c.migrationsTableWithSchema() + ` (version bigint not null primary key, dirty boolean not null)`
 	if _, err = c.conn.ExecContext(context.Background(), query); err != nil {
 		return &database.Error{OrigErr: err, Query: []byte(query)}
 	}
 
 	return nil
+}
+
+func (c *Crate) ensureLockTable() error {
+	query := `CREATE TABLE IF NOT EXISTS ` + pq.QuoteIdentifier(c.config.MigrationsSchemaName) + "." + pq.QuoteIdentifier(c.config.LockTableName) + ` (lock_id bigint not null primary key)`
+	_, err := c.conn.ExecContext(context.Background(), query)
+	return err
+}
+
+func (c *Crate) migrationsTableWithSchema() string {
+	return pq.QuoteIdentifier(c.config.MigrationsSchemaName) + "." + pq.QuoteIdentifier(c.config.MigrationsTableName)
 }
